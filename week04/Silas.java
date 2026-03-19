05d3d1b584fa8941657d4de62e607ef86744b1f7
Xiang1032 <c111181141@gmail.com>
2026-03-19 11:18:31 +0800

    0319

---
+++ week04/Silas.java
@@ -0,0 +1,18 @@
+
+public class Silas
+{
+    // An example method
+    public void printMessage() {
+        System.out.println("Hello, Silas!");
+    }
+    
+    public static void main(String[] args) {
+        
+        // Create an instance of the class
+        // containing the method
+        Silas obj = new Silas();
+        
+        // Calling the method
+        obj.printMessage(); 
+    }
+}
